1c0d941dace131879c56e51069addf7e37eafed3
Barathlingam K <rkbarath5717@gmail.com>
2026-08-13 21:14:15 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[]= new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println("After sorting the array : " + arr[i]);
+        }
+    }
+}
