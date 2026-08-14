62717ceeb64cebd05c271a293cf8b77ca0255c80
Barathlingam K <rkbarath5717@gmail.com>
2026-08-14 21:46:11 +0530

    Add files via upload

---
+++ SortOnlyEven.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class SortOnlyEven {
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        for(int i=1;i<n;i++){
+            if(arr[i] %2 == 0){
+                int key = arr[i];
+                int j=i-1;
+                while(j>=0 && arr[j] > key){
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                arr[j+1]=key;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
